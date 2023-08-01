4d43cd885d56f8f938a8faef885519fc38bcddf4
erdalcelebi <clberdal0@gmail.com>
2023-08-02 00:13:31 +0300

    4.versiyon

--- src/githubDay01_2.java
+++ src/githubDay01_2.java
@@ -1,5 +1,6 @@
 public class githubDay01_2 {
     public static void main(String[] args) {
         System.out.println("iŞTE ŞİMDİ OLDU");
+        System.out.println("2. dosya birinci satır");
     }
 }
